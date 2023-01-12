decf4083d6d2479901be2bdca5448a4cdb15286a
Elizabeth Villarruel Rosales <108765164+ellyhp@users.noreply.github.com>
2023-01-12 10:06:41 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+    //Responder en esta variable
+    $segundos_Conversion = 7400;
+
+    echo "¿Cuántos segundos quieres convertir a minutos? <br>";
+
+    $Conversion = (7400 / 60); 
+    $Conversion = (int)$Conversion;
+    echo "La conversión es:".$Conversion." "."Minutos";
+?>
